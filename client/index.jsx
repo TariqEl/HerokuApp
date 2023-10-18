935b20d23df903d037548c12725741dde0b5adf6
TariqEl <123506602+TariqEl@users.noreply.github.com>
2023-10-18 19:52:11 +0200

    Added again

--- client/index.jsx
+++ client/index.jsx
@@ -5,12 +5,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Application />);
 
 function Application() {
-  const [counter, setCounter] = useState(0);
   return <>
     <h2>Welcome to my application</h2>
     <div>
-      <button onClick={() => setCounter(oldValue => oldValue + 1)}>Click me</button>
+      <h2>Hello</h2>
     </div>
-    <div>You have clicked {counter} times</div>
   </>;
 }
